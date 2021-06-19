d3eafa1318b32307bdc3acaee370cb51deb3b022
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-06-18 20:57:00 -0400

    implement reflection modification methods

--- src/main/java/codes/ttp/momoyo/Momoyo.java
+++ src/main/java/codes/ttp/momoyo/Momoyo.java
@@ -9,6 +9,10 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 @Mod(
         modid = Momoyo.MOD_ID,
@@ -20,6 +24,8 @@ public class Momoyo {
     public static final String MOD_ID = "momoyo";
     public static final String MOD_NAME = "Momoyo";
     public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
@@ -33,7 +39,8 @@ public class Momoyo {
      */
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-
+        logger = event.getModLog();
+        logger.info("Logging started");
     }
 
     /**
@@ -50,6 +57,56 @@ public class Momoyo {
     @Mod.EventHandler
     public void postinit(FMLPostInitializationEvent event) {
 
+    }
+
+    private void setHarvestLevel(String name, int level, Class<?> materials) {
+        Field materialField;
+        try {
+            materialField = materials.getField(name);
+        } catch (NoSuchFieldException e) {
+            logger.fatal("{} was not detected as a material", name);
+            throw new RuntimeException(e);
+        }
+
+        Object material;
+        try {
+            material = materialField.get(materials);
+        } catch (IllegalAccessException e) {
+            logger.fatal("Momoyo is forbidden from accessing {} :(", name);
+            throw new RuntimeException(e);
+        }
+
+        Field harvestLevel;
+        try {
+            harvestLevel = material.getClass().getSuperclass().getSuperclass().getField("harvestLevel");
+        } catch (NoSuchFieldException e) {
+            logger.fatal("Could not get harvestLevel field");
+            throw new RuntimeException(e);
+        }
+
+        Field modifiers;
+        try {
+            modifiers = harvestLevel.getClass().getDeclaredField("modifiers");
+        } catch (NoSuchFieldException e) {
+            logger.fatal("Could not access modifiers of harvestLevel");
+            throw new RuntimeException(e);
+        }
+
+        modifiers.setAccessible(true);
+        try {
+            modifiers.setInt(harvestLevel, harvestLevel.getModifiers() & ~Modifier.FINAL);
+        } catch (IllegalAccessException e) {
+            logger.fatal("Could not set harvestLevel as not final");
+            throw new RuntimeException(e);
+        }
+
+        try {
+            harvestLevel.set(material, level);
+        } catch (IllegalAccessException e) {
+            logger.fatal("Could not set harvestLevel of {}", name);
+            throw new RuntimeException(e);
+        }
+        logger.info("harvestLevel of {} changed to {}", name, level);
     }
 
     /**
